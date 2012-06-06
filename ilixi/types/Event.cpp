9ca72f4d335ea04148912e4949f20e6bb45fdd22
Tarik Sekmen <tarik@directfb.org>
2012-06-06 17:49:38 +0200

    Added missing source file.

---
+++ ilixi/types/Event.cpp
@@ -0,0 +1,49 @@
+/*
+ Copyright 2011 Tarik Sekmen.
+
+ All Rights Reserved.
+
+ Written by Tarik Sekmen <tarik@ilixi.org>.
+
+ This file is part of ilixi.
+
+ ilixi is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ ilixi is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with ilixi.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "types/Event.h"
+#include "ui/Widget.h"
+
+namespace ilixi
+{
+#ifdef ILIXI_STEREO_OUTPUT
+  PaintEvent::PaintEvent(Widget* widget, const PaintEvent& evt) :
+      right(), rect(), eye(evt.eye)
+  {
+    widget->updateSurface(evt);
+    Rectangle lt = widget->frameGeometry();
+    lt.translate(widget->z(), 0);
+    Rectangle rt = widget->frameGeometry();
+    rt.translate(-widget->z(), 0);
+    rect = lt.intersected(evt.rect);
+    right = rt.intersected(evt.right);
+  }
+#else
+  PaintEvent::PaintEvent(Widget* widget, const PaintEvent& evt) :
+  rect(), eye(evt.eye)
+    {
+      widget->updateSurface(evt);
+      rect = widget->frameGeometry().intersected(evt.rect);
+    }
+#endif
+}
