09e80354031f55dc0eb0c86f8973dc421ebdc8b1
Tarik Sekmen <tarik@directfb.org>
2012-08-23 18:39:01 +0200

    License for VolumeMeter widget :)

--- bin/soundmixer/VolumeMeter.cpp
+++ bin/soundmixer/VolumeMeter.cpp
@@ -1,13 +1,29 @@
 /*
- * VolumeMeter.cpp
- *
- *  Created on: Aug 23, 2012
- *      Author: tarik
+ Copyright 2010-2012 Tarik Sekmen.
+
+ All Rights Reserved.
+
+ Written by Tarik Sekmen <tarik@ilixi.org>,
+            Denis Oliver Kropp <dok@directfb.org>.
+
+ This file is part of ilixi.
+
+ ilixi is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ ilixi is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with ilixi.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "VolumeMeter.h"
 #include <graphics/Painter.h>
-#include <core/Logger.h>
 
 namespace ilixi
 {
@@ -56,67 +72,14 @@ VolumeMeter::drawMeterValue(Painter* p, int x, float value)
 
     split = (split + 11) / 12 * 12;
 
-    p->blitImage(_volumeOn, Rectangle(0, 304 - 3 - split, 32, 3 + split), x, 304 - 3 - split);
-
-/*
-    if (value < 0.08)
-        p->blitImage(_volumeOn, Rectangle(0, 289, 32, 15), x, 289);
-    else if (value < 0.16)
-        p->blitImage(_volumeOn, Rectangle(0, 277, 32, 27), x, 277);
-    else if (value < 0.24)
-        p->blitImage(_volumeOn, Rectangle(0, 265, 32, 39), x, 265);
-    else if (value < 0.32)
-        p->blitImage(_volumeOn, Rectangle(0, 253, 32, 51), x, 253);
-    else if (value < 0.40)
-        p->blitImage(_volumeOn, Rectangle(0, 241, 32, 63), x, 241);
-    else if (value < 0.48)
-        p->blitImage(_volumeOn, Rectangle(0, 229, 32, 75), x, 229);
-    else if (value < 0.56)
-        p->blitImage(_volumeOn, Rectangle(0, 217, 32, 87), x, 217);
-    else if (value < 0.64)
-        p->blitImage(_volumeOn, Rectangle(0, 265, 32, 39), x, 265);
-    else if (value < 0.72)
-        p->blitImage(_volumeOn, Rectangle(0, 265, 32, 39), x, 265);
-    else if (value < 0.80)
-        p->blitImage(_volumeOn, Rectangle(0, 265, 32, 39), x, 265);
-    else if (value < 0.88)
-        p->blitImage(_volumeOn, Rectangle(0, 265, 32, 39), x, 265);
-    else if (value < 0.96)
-        p->blitImage(_volumeOn, Rectangle(0, 265, 32, 39), x, 265);
-    else if (value < 1.04)
-        p->blitImage(_volumeOn, Rectangle(0, 265, 32, 39), x, 265);
-    else if (value < 1.12)
-        p->blitImage(_volumeOn, Rectangle(0, 265, 32, 39), x, 265);
-    else if (value < 1.20)
-        p->blitImage(_volumeOn, Rectangle(0, 265, 32, 39), x, 265);
-    else if (value < 1.28)
-        p->blitImage(_volumeOn, Rectangle(0, 265, 32, 39), x, 265);
-    else if (value < 1.36)
-        p->blitImage(_volumeOn, Rectangle(0, 265, 32, 39), x, 265);
-    else if (value < 1.44)
-        p->blitImage(_volumeOn, Rectangle(0, 265, 32, 39), x, 265);
-    else if (value < 1.52)
-        p->blitImage(_volumeOn, Rectangle(0, 265, 32, 39), x, 265);
-    else if (value < 1.60)
-        p->blitImage(_volumeOn, Rectangle(0, 265, 32, 39), x, 265);
-    else if (value < 1.68)
-        p->blitImage(_volumeOn, Rectangle(0, 265, 32, 39), x, 265);
-    else if (value < 1.76)
-        p->blitImage(_volumeOn, Rectangle(0, 265, 32, 39), x, 265);
-    else if (value < 1.84)
-        p->blitImage(_volumeOn, Rectangle(0, 265, 32, 39), x, 265);
-    else if (value < 1.82)
-        p->blitImage(_volumeOn, Rectangle(0, 265, 32, 39), x, 265);
-    else
-        p->blitImage(_volumeOn, Rectangle(0, 265, 32, 39), x, 265);
-*/
+    p->blitImage(_volumeOn, Rectangle(0, 304 - 3 - split, 32, 3 + split), x,
+                 304 - 3 - split);
 }
 
 void
 VolumeMeter::updateVolume()
 {
     _sound.getMasterAmplitude(&_chanLeft, &_chanRight);
-    ILOG_INFO(ILX, "%f - %f\n", _chanLeft, _chanRight);
     update();
 }
 
