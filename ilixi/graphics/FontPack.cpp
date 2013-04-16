14e542405e0288d2332f45d2dff98a26f9d36116
Tarik Sekmen <tarik@directfb.org>
2013-04-16 03:45:05 +0300

    Add FontPack class to parse default fonts.

---
+++ ilixi/graphics/FontPack.cpp
@@ -0,0 +1,193 @@
+/*
+ Copyright 2010-2012 Tarik Sekmen.
+
+ All Rights Reserved.
+
+ Written by Tarik Sekmen <tarik@ilixi.org>.
+
+ This file is part of ilixi.
+
+ ilixi is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ ilixi is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with ilixi.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <graphics/FontPack.h>
+#include <lib/FileSystem.h>
+#include <lib/XMLReader.h>
+#include <core/Logger.h>
+#include <fstream>
+
+namespace ilixi
+{
+
+D_DEBUG_DOMAIN(ILX_FONTPACK, "ilixi/graphics/FontPack", "FontPack");
+
+FontPack::FontPack()
+        : _buttonFont(NULL),
+          _defaultFont(NULL),
+          _inputFont(NULL),
+          _titleFont(NULL)
+{
+}
+
+FontPack::~FontPack()
+{
+    release();
+}
+
+Font*
+FontPack::getFont(StyleHint::Font font) const
+{
+    switch (font)
+    {
+    case StyleHint::ButtonFont:
+        return _buttonFont;
+    case StyleHint::TitleFont:
+        return _titleFont;
+    case StyleHint::InputFont:
+        return _inputFont;
+    default:
+        return _defaultFont;
+    }
+}
+
+bool
+FontPack::parseFonts(const char* fontsFile)
+{
+    ILOG_TRACE(ILX_FONTPACK);
+    ILOG_DEBUG(ILX_FONTPACK, " -> file: %s\n", fontsFile);
+
+    std::string cacheFile = FileSystem::homeDirectory().append(PrintF("/%u.sxml", createHash(fontsFile)));
+    if (difftime(FileSystem::getModificationTime(cacheFile), FileSystem::getModificationTime(fontsFile)) > 0)
+    {
+        ILOG_DEBUG(ILX_FONTPACK, " -> Parsing cached fonts file.\n");
+        std::ifstream ifs(cacheFile.c_str(), std::ios::in);
+        ifs >> *this;
+        ifs.close();
+        ILOG_INFO(ILX_FONTPACK, "Parsed cached fonts file: %s\n", cacheFile.c_str());
+    } else
+    {
+        XMLReader xml;
+        if (xml.loadFile(fontsFile) == false)
+        {
+            ILOG_FATAL(ILX_FONTPACK, "Could not parse fonts!\n");
+            return false;
+        }
+
+        xmlNodePtr node = xml.currentNode();
+
+        release();
+
+        while (node != NULL)
+        {
+            ILOG_DEBUG(ILX_FONTPACK, " -> font: %s...\n", node->name);
+            xmlChar* fileC = xmlNodeGetContent(node->children);
+            xmlChar* sizeC = xmlNodeGetContent(node->children->next);
+            xmlChar* styleC = xmlNodeGetContent(node->children->next->next);
+
+            Font::Style fontStyle = Font::Plain;
+            if (styleC)
+            {
+                if (xmlStrcmp(styleC, (xmlChar *) "italic") == 0)
+                    fontStyle = Font::Italic;
+                else if (xmlStrcmp(styleC, (xmlChar *) "bold") == 0)
+                    fontStyle = Font::Bold;
+                xmlFree(styleC);
+            }
+
+            if (xmlStrcmp(node->name, (xmlChar*) "DefaultFont") == 0)
+            {
+                _defaultFont = new Font((char*) fileC, atoi((char*) sizeC));
+                _defaultFont->setStyle(fontStyle);
+            } else if (xmlStrcmp(node->name, (xmlChar*) "ButtonFont") == 0)
+            {
+                _buttonFont = new Font((char*) fileC, atoi((char*) sizeC));
+                _buttonFont->setStyle(fontStyle);
+            }
+
+            else if (xmlStrcmp(node->name, (xmlChar*) "InputFont") == 0)
+            {
+                _inputFont = new Font((char*) fileC, atoi((char*) sizeC));
+                _inputFont->setStyle(fontStyle);
+            }
+
+            else if (xmlStrcmp(node->name, (xmlChar*) "TitleFont") == 0)
+            {
+                _titleFont = new Font((char*) fileC, atoi((char*) sizeC));
+                _titleFont->setStyle(fontStyle);
+            }
+
+            xmlFree(fileC);
+            xmlFree(sizeC);
+            node = node->next;
+        }
+        ILOG_INFO(ILX_FONTPACK, "Parsed fonts file: %s\n", fontsFile);
+
+        std::ofstream ofs(cacheFile.c_str(), std::ios::out);
+        ofs.seekp(0, std::ios::beg);
+        ofs << *this;
+        ofs.close();
+        ILOG_INFO(ILX_FONTPACK, "Created cached fonts file: %s\n", cacheFile.c_str());
+    }
+
+    return true;
+}
+
+void
+FontPack::release()
+{
+    delete _buttonFont;
+    delete _defaultFont;
+    delete _inputFont;
+    delete _titleFont;
+
+    _buttonFont = NULL;
+    _defaultFont = NULL;
+    _inputFont = NULL;
+    _titleFont = NULL;
+}
+
+std::istream&
+operator>>(std::istream& is, FontPack& obj)
+{
+    obj.release();
+
+    obj._buttonFont = new Font();
+    obj._defaultFont = new Font();
+    obj._inputFont = new Font();
+    obj._titleFont = new Font();
+
+    is >> *obj._buttonFont;
+    is.ignore(1);
+    is >> *obj._defaultFont;
+    is.ignore(1);
+    is >> *obj._inputFont;
+    is.ignore(1);
+    is >> *obj._titleFont;
+    is.ignore(1);
+
+    return is;
+}
+
+std::ostream&
+operator<<(std::ostream& os, const FontPack& obj)
+{
+    os << *obj._buttonFont << std::endl;
+    os << *obj._defaultFont << std::endl;
+    os << *obj._inputFont << std::endl;
+    os << *obj._titleFont << std::endl;
+
+    return os;
+}
+
+} /* namespace ilixi */
---
+++ ilixi/graphics/FontPack.h
@@ -0,0 +1,84 @@
+/*
+ Copyright 2010-2012 Tarik Sekmen.
+
+ All Rights Reserved.
+
+ Written by Tarik Sekmen <tarik@ilixi.org>.
+
+ This file is part of ilixi.
+
+ ilixi is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ ilixi is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with ilixi.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ILIXI_FONTS_H_
+#define ILIXI_FONTS_H_
+
+#include <types/Font.h>
+#include <types/Enums.h>
+#include <iostream>
+
+namespace ilixi
+{
+//! Provides a font pack.
+class FontPack
+{
+public:
+    /*!
+     * Constructor.
+     */
+    FontPack();
+
+    /*!
+     * Destructor.
+     */
+    virtual
+    ~FontPack();
+
+    /*!
+     * Returns the font for given type.
+     */
+    Font*
+    getFont(StyleHint::Font font = StyleHint::DefaultFont) const;
+
+    /*!
+     * Initialise fonts from an XML file.
+     *
+     * Returns false on error.
+     */
+    bool
+    parseFonts(const char* fontsFile);
+
+private:
+    //! Font for rendering button text.
+    Font* _buttonFont;
+    //! Default font to render text.
+    Font* _defaultFont;
+    //! Font for rendering input text.
+    Font* _inputFont;
+    //! Font for rendering title text.
+    Font* _titleFont;
+
+    //! Release fonts.
+    void
+    release();
+
+    friend std::istream&
+    operator>>(std::istream& is, FontPack& obj);
+
+    friend std::ostream&
+    operator<<(std::ostream& os, const FontPack& obj);
+};
+
+} /* namespace ilixi */
+#endif /* ILIXI_FONTS_H_ */
