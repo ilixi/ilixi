b27372fce8bbb446225497f4acf12a00f8f9a1ad
Tarik Sekmen <tarik@directfb.org>
2013-12-16 16:34:21 +0200

    swig: Fix Dialogs.cs example.

--- swig/csharp/Dialogs.cs
+++ swig/csharp/Dialogs.cs
@@ -11,24 +11,22 @@ class MainClass
 
         Dialog dialog1 = new Dialog("Dialog 1", Dialog.ButtonOption.NoButtonOption);
         ToolButton button1 = new ToolButton("Show dialog 1");
-        DelegateNotify a = new DelegateNotify(dialog1.execute);
-        button1.sigPressed.connect(button1, a);
+        button1.connectClick(new DelegateNotify(dialog1.execute));
         app.addWidget(button1);
 
         Dialog dialog2 = new Dialog("Dialog 2", Dialog.ButtonOption.CancelButtonOption);
         ToolButton button2 = new ToolButton("Show dialog 2");
-        DelegateNotify b = new DelegateNotify(dialog2.execute);
-        button2.sigClicked.connect(button2, b);
+        button2.connectClick(new DelegateNotify(dialog2.execute));
         app.addWidget(button2);
 
         Dialog dialog3 = new Dialog("Dialog 3", Dialog.ButtonOption.OKCancelButtonOption);
         ToolButton button3 = new ToolButton("Show dialog 3");
-        button3.sigClicked.connect(button3, dialog3.execute);
+        button3.connectClick(new DelegateNotify(dialog3.execute));
         app.addWidget(button3);
 
         Dialog dialog4 = new Dialog("Dialog 4", Dialog.ButtonOption.YesNoCancelButtonOption);
         ToolButton button4 = new ToolButton("Show dialog 4");
-        button4.sigPressed.connect(button4, dialog4.execute);
+        button4.connectClick(new DelegateNotify(dialog4.execute));
         app.addWidget(button4);
 
         app.exec();
